1518da99c2df5c7eb43f5039311bdb13422e1843
Ricardo Bánffy <rbanffy@gmail.com>
2021-11-10 22:13:23 +0000

    Change terminal window title, wait before returning

--- lines.py
+++ lines.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python3
 
 import random
+import time
 
 from tektronix import tek_mode, vt100_mode, clear, line
 
 
 if __name__ == '__main__':
+    print("\033]0;Fun with Tektronix\007")
     tek_mode()
     clear()
     for _ in range(200):
         line(
             random.randint(0, 1023), random.randint(0, 767),
             random.randint(0, 1023), random.randint(0, 767))
+        time.sleep(.05)
+    time.sleep(5)
     vt100_mode()
