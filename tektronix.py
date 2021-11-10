e6f59a27db61cb0e1f2eee7b481adc80a669e2d1
Ricardo Bánffy <rbanffy@gmail.com>
2021-11-10 15:18:15 +0000

    Use Python 3 syntax, add  tek_mode and vt100_mode

--- tektronix.py
+++ tektronix.py
@@ -1,14 +1,26 @@
 GS = chr(29)
 US = chr(31)
 ESC = chr(27)
+ETX = chr(3)
 FF = chr(12)
+CSI = ESC + "["
 
 
+def tek_mode():
+    "Enter Tektronix mode"
+    print(CSI + "38h")
+
+def vt100_mode():
+    "Goes back to VT100 mode"
+    print(ESC + ETX)
+        
 def clear():
-    print ESC + FF
+    "Clears the screen. On a real Tektronix, this flashes the screen white"
+    print(ESC + FF)
 
 
 def coord(x, y):
+    "Translates coordinates to Tek strings"
     if x < 0 or x > 1024 or y < 0 or y > 768:
         raise ValueError("Invalid coordinates (%d, %d)" % (x, y))
     return chr(32 + (y // 32)) + chr(96 + (y & 31)) + chr(32 + x // 32) \
@@ -16,4 +28,5 @@ def coord(x, y):
 
 
 def line(x1, y1, x2, y2):
-    print GS + ESC + "[" + coord(x1, y1) + coord(x2, y2) + US
+    "Draws a line from *x1, y1) to (x2, y2)"
+    print(GS + ESC + "[" + coord(x1, y1) + coord(x2, y2) + US)
